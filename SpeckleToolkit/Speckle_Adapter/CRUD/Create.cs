3612040792c046d1e2d1896b32e24cd17e618446
Dimitrie Stefanescu <didimitrie@gmail.com>
2019-02-20 16:02:39 +0000

    removed submodules and scaffolded some stuff

--- SpeckleToolkit/Speckle_Adapter/CRUD/Create.cs
+++ SpeckleToolkit/Speckle_Adapter/CRUD/Create.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using BH.oM.Structural.Elements;
-using BH.oM.Structural.Properties;
-using BH.oM.Common.Materials;
 
 namespace BH.Adapter.Speckle
 {
@@ -15,89 +12,89 @@ namespace BH.Adapter.Speckle
     /**** Adapter overload method                   ****/
     /***************************************************/
 
-    protected override bool Create<T>( IEnumerable<T> objects, bool replaceAll = false )
-    {
-      bool success = true;        //boolean returning if the creation was successfull or not
+    //protected override bool Create<T>( IEnumerable<T> objects, bool replaceAll = false )
+    //{
+    //  bool success = true;        //boolean returning if the creation was successfull or not
 
-      success = CreateCollection( objects as dynamic );
+    //  success = CreateCollection( objects as dynamic );
 
-      //UpdateViews()             //If there exists a command for updating the views is the software call it now:
+    //  UpdateViews()             //If there exists a command for updating the views is the software call it now:
 
-      return success;             //Finally return if the creation was successful or not
+    //  return success;             //Finally return if the creation was successful or not
 
-    }
+    //}
 
-    /***************************************************/
-    /**** Private methods                           ****/
-    /***************************************************/
+    ///***************************************************/
+    ///**** Private methods                           ****/
+    ///***************************************************/
 
-    private bool CreateCollection( IEnumerable<Bar> bars )
-    {
-      //Code for creating a collection of bars in the software
-
-
-      foreach ( Bar bar in bars )
-      {
-        //Tip: if the NextId method has been implemented you can get the id to be used for the creation out as (cast into applicable type used by the software):
-        object barId = bar.CustomData[ AdapterId ];
-        //If also the default implmentation for the DependencyTypes is used,
-        //one can from here get the id's of the subobjects by calling (cast into applicable type used by the software): 
-        object startNodeId = bar.StartNode.CustomData[ AdapterId ];
-        object endNodeId = bar.EndNode.CustomData[ AdapterId ];
-        object SecPropId = bar.SectionProperty.CustomData[ AdapterId ];
-      }
+    //private bool CreateCollection( IEnumerable<Bar> bars )
+    //{
+    //  //Code for creating a collection of bars in the software
 
 
-      throw new NotImplementedException();
-    }
+    //  foreach ( Bar bar in bars )
+    //  {
+    //    //Tip: if the NextId method has been implemented you can get the id to be used for the creation out as (cast into applicable type used by the software):
+    //    object barId = bar.CustomData[ AdapterId ];
+    //    //If also the default implmentation for the DependencyTypes is used,
+    //    //one can from here get the id's of the subobjects by calling (cast into applicable type used by the software): 
+    //    object startNodeId = bar.StartNode.CustomData[ AdapterId ];
+    //    object endNodeId = bar.EndNode.CustomData[ AdapterId ];
+    //    object SecPropId = bar.SectionProperty.CustomData[ AdapterId ];
+    //  }
 
-    /***************************************************/
 
-    private bool CreateCollection( IEnumerable<Node> nodes )
-    {
-      //Code for creating a collection of nodes in the software
+    //  throw new NotImplementedException();
+    //}
 
-      foreach ( Node node in nodes )
-      {
-        //Tip: if the NextId method has been implemented you can get the id to be used for the creation out as (cast into applicable type used by the software):
-        object nodeId = node.CustomData[ AdapterId ];
-      }
+    ///***************************************************/
 
-      throw new NotImplementedException();
-    }
+    //private bool CreateCollection( IEnumerable<Node> nodes )
+    //{
+    //  //Code for creating a collection of nodes in the software
 
-    /***************************************************/
+    //  foreach ( Node node in nodes )
+    //  {
+    //    //Tip: if the NextId method has been implemented you can get the id to be used for the creation out as (cast into applicable type used by the software):
+    //    object nodeId = node.CustomData[ AdapterId ];
+    //  }
 
-    private bool CreateCollection( IEnumerable<ISectionProperty> sectionProperties )
-    {
-      //Code for creating a collection of section properties in the software
+    //  throw new NotImplementedException();
+    //}
 
-      foreach ( ISectionProperty sectionProperty in sectionProperties )
-      {
-        //Tip: if the NextId method has been implemented you can get the id to be used for the creation out as (cast into applicable type used by the software):
-        object secPropId = sectionProperty.CustomData[ AdapterId ];
-        //If also the default implmentation for the DependencyTypes is used,
-        //one can from here get the id's of the subobjects by calling (cast into applicable type used by the software): 
-        object materialId = sectionProperty.Material.CustomData[ AdapterId ];
-      }
+    ///***************************************************/
 
-      throw new NotImplementedException();
-    }
+    //private bool CreateCollection( IEnumerable<ISectionProperty> sectionProperties )
+    //{
+    //  //Code for creating a collection of section properties in the software
 
-    /***************************************************/
+    //  foreach ( ISectionProperty sectionProperty in sectionProperties )
+    //  {
+    //    //Tip: if the NextId method has been implemented you can get the id to be used for the creation out as (cast into applicable type used by the software):
+    //    object secPropId = sectionProperty.CustomData[ AdapterId ];
+    //    //If also the default implmentation for the DependencyTypes is used,
+    //    //one can from here get the id's of the subobjects by calling (cast into applicable type used by the software): 
+    //    object materialId = sectionProperty.Material.CustomData[ AdapterId ];
+    //  }
 
-    private bool CreateCollection( IEnumerable<Material> materials )
-    {
-      //Code for creating a collection of materials in the software
+    //  throw new NotImplementedException();
+    //}
 
-      foreach ( Material material in materials )
-      {
-        //Tip: if the NextId method has been implemented you can get the id to be used for the creation out as (cast into applicable type used by the software):
-        object materialId = material.CustomData[ AdapterId ];
-      }
+    ///***************************************************/
 
-      throw new NotImplementedException();
-    }
+    //private bool CreateCollection( IEnumerable<Material> materials )
+    //{
+    //  //Code for creating a collection of materials in the software
+
+    //  foreach ( Material material in materials )
+    //  {
+    //    //Tip: if the NextId method has been implemented you can get the id to be used for the creation out as (cast into applicable type used by the software):
+    //    object materialId = material.CustomData[ AdapterId ];
+    //  }
+
+    //  throw new NotImplementedException();
+    //}
 
 
     /***************************************************/
