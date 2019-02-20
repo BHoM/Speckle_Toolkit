dae1bf1b5379810dde10d0e9848f8685b7281f8f
Dimitrie Stefanescu <didimitrie@gmail.com>
2019-02-20 12:19:25 +0000

    scaffolding

---
+++ SpeckleToolkit/Speckle_Engine/Convert/ToBHoM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Structural.Elements;
+
+namespace BH.Engine.Speckle
+{
+  public static partial class Convert
+  {
+    /***************************************************/
+    /**** Public Methods                            ****/
+    /***************************************************/
+
+    //Add methods for converting to BHoM from the specific software types, if possible to do without any BHoM calls
+    //Example:
+    //public static Node ToBHoM(this SpeckleNode node)
+    //{
+    //    //Insert code for convertion
+    //}
+
+    /***************************************************/
+  }
+}
