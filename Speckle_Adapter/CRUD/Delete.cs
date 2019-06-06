5ba7702bdcd83280c96eae9a2c2092f41a35d7e9
Alessio Lombardi <alessio.lombardi@burohappold.com>
2019-06-06 17:04:08 +0100

    Reorganized folders

---
+++ Speckle_Adapter/CRUD/Delete.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Adapter.Speckle
+{
+  public partial class SpeckleAdapter
+  {
+    /***************************************************/
+    /**** Adapter overload method                   ****/
+    /***************************************************/
+
+    protected override int Delete( Type type, IEnumerable<object> ids )
+    {
+      //Insert code here to enable deletion of specific types of objects with specific ids
+      return 0;
+    }
+
+    /***************************************************/
+  }
+}
---
+++ Speckle_Adapter/Types/DependencyTypes.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using BH.oM.Base;
+
+namespace BH.Adapter.Speckle
+{
+  public partial class SpeckleAdapter
+  {
+
+    /***************************************************/
+    /**** BHoM Adapter Interface                    ****/
+    /***************************************************/
+
+    //Standard implementation for dependency types (change the dictionary below to override):
+
+    protected override List<Type> DependencyTypes<T>( )
+    {
+      //Type type = typeof( T );
+
+      //if ( m_DependencyTypes.ContainsKey( type ) )
+      //  return m_DependencyTypes[ type ];
+
+      //else if ( type.BaseType != null && m_DependencyTypes.ContainsKey( type.BaseType ) )
+      //  return m_DependencyTypes[ type.BaseType ];
+
+      //else
+      //{
+      //  foreach ( Type interType in type.GetInterfaces() )
+      //  {
+      //    if ( m_DependencyTypes.ContainsKey( interType ) )
+      //      return m_DependencyTypes[ interType ];
+      //  }
+      //}
+
+
+      return new List<Type>();
+    }
+
+    /***************************************************/
+    /**** Private Fields                            ****/
+    /***************************************************/
+
+    //private static Dictionary<Type, List<Type>> m_DependencyTypes = new Dictionary<Type, List<Type>>
+    //    {
+    //        {typeof(Bar), new List<Type> { typeof(ISectionProperty), typeof(Node) } },
+    //        {typeof(ISectionProperty), new List<Type> { typeof(Material) } },
+    //        {typeof(RigidLink), new List<Type> { typeof(LinkConstraint), typeof(Node) } },
+    //        {typeof(MeshFace), new List<Type> { typeof(Property2D), typeof(Node) } },
+    //        {typeof(Property2D), new List<Type> { typeof(Material) } },
+    //        {typeof(PanelPlanar), new List<Type> { typeof(Property2D) } }
+    //    };
+
+
+    /***************************************************/
+  }
+}
