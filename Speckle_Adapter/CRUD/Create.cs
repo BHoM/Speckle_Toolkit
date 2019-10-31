3edd05bf6f8de12757f690bfece2dc84bb02f011
Alessio Lombardi <alessio.lombardi@burohappold.com>
2019-10-31 16:41:03 +0000

    #37

--- Speckle_Adapter/CRUD/Create.cs
+++ Speckle_Adapter/CRUD/Create.cs
@@ -17,15 +17,15 @@ namespace BH.Adapter.Speckle
         /**** Adapter overload method                   ****/
         /***************************************************/
 
-        protected override bool Create<T>(IEnumerable<T> objects, bool replaceAll = false)
+        protected override bool Create<T>(IEnumerable<T> objects)
         {
             // This method gets always called by the base.Push --> objects are always IBHoMObjects
 
             //IEnumerable<IBHoMObject> bHoMObjects = objects.Cast<IBHoMObject>();
-            return CreateIBHoMObjects(objects as dynamic, replaceAll);
+            return CreateIBHoMObjects(objects as dynamic);
         }
 
-        protected bool CreateIObjects(IEnumerable<IObject> objects, bool replaceAll = false)
+        protected bool CreateIObjects(IEnumerable<IObject> objects)
         {
             IEnumerable<object> newObjects = (IEnumerable<object>)objects; //hacky; assumes T is always a reference type. Should be no problem anyway
 
@@ -39,6 +39,9 @@ namespace BH.Adapter.Speckle
             return true;
         }
 
+        // Note: setAssignedId is currently not exposed as an option
+        //  -- waiting for Adapter refactoring LVL 04 to expose a new CRUDconfig input for the Push 
+        // CRUDconfig will become available to all CRUD methods
         protected bool CreateIBHoMObjects(IEnumerable<IBHoMObject> objects, bool setAssignedId = true)
         {
             /// Create the objects
@@ -86,6 +89,9 @@ namespace BH.Adapter.Speckle
             return true;
         }
 
+        // Note: setAssignedId is currently not exposed as an option
+        //  -- waiting for Adapter refactoring LVL 04 to expose a new CRUDconfig input for the Push 
+        // CRUDconfig will become available to all CRUD methods
         protected bool CreateAnyObject(List<IObject> objects, bool setAssignedId = true)
         {
             /// Create the objects
@@ -137,78 +143,6 @@ namespace BH.Adapter.Speckle
 
             return true;
         }
-
-        ///***************************************************/
-        ///**** Private methods                           ****/
-        ///***************************************************/
-
-        //private bool CreateCollection( IEnumerable<Bar> bars )
-        //{
-        //  //Code for creating a collection of bars in the software
-
-
-        //  foreach ( Bar bar in bars )
-        //  {
-        //    //Tip: if the NextId method has been implemented you can get the id to be used for the creation out as (cast into applicable type used by the software):
-        //    object barId = bar.CustomData[ AdapterId ];
-        //    //If also the default implmentation for the DependencyTypes is used,
-        //    //one can from here get the id's of the subobjects by calling (cast into applicable type used by the software): 
-        //    object startNodeId = bar.StartNode.CustomData[ AdapterId ];
-        //    object endNodeId = bar.EndNode.CustomData[ AdapterId ];
-        //    object SecPropId = bar.SectionProperty.CustomData[ AdapterId ];
-        //  }
-
-
-        //  throw new NotImplementedException();
-        //}
-
-        ///***************************************************/
-
-        //private bool CreateCollection( IEnumerable<Node> nodes )
-        //{
-        //  //Code for creating a collection of nodes in the software
-
-        //  foreach ( Node node in nodes )
-        //  {
-        //    //Tip: if the NextId method has been implemented you can get the id to be used for the creation out as (cast into applicable type used by the software):
-        //    object nodeId = node.CustomData[ AdapterId ];
-        //  }
-
-        //  throw new NotImplementedException();
-        //}
-
-        ///***************************************************/
-
-        //private bool CreateCollection( IEnumerable<ISectionProperty> sectionProperties )
-        //{
-        //  //Code for creating a collection of section properties in the software
-
-        //  foreach ( ISectionProperty sectionProperty in sectionProperties )
-        //  {
-        //    //Tip: if the NextId method has been implemented you can get the id to be used for the creation out as (cast into applicable type used by the software):
-        //    object secPropId = sectionProperty.CustomData[ AdapterId ];
-        //    //If also the default implmentation for the DependencyTypes is used,
-        //    //one can from here get the id's of the subobjects by calling (cast into applicable type used by the software): 
-        //    object materialId = sectionProperty.Material.CustomData[ AdapterId ];
-        //  }
-
-        //  throw new NotImplementedException();
-        //}
-
-        ///***************************************************/
-
-        //private bool CreateCollection( IEnumerable<Material> materials )
-        //{
-        //  //Code for creating a collection of materials in the software
-
-        //  foreach ( Material material in materials )
-        //  {
-        //    //Tip: if the NextId method has been implemented you can get the id to be used for the creation out as (cast into applicable type used by the software):
-        //    object materialId = material.CustomData[ AdapterId ];
-        //  }
-
-        //  throw new NotImplementedException();
-        //}
 
 
         /***************************************************/
