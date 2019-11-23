02ba6614f8e248310eac404d0297946d9bd891d3
unknown <izzy.lyseggen@gmail.com>
2019-11-23 13:25:19 +0000

    Adapt `ToPoints` helper function

--- Speckle_Engine/Convert/ToBHoM.cs
+++ Speckle_Engine/Convert/ToBHoM.cs
@@ -1,18 +1,38 @@
-﻿using BH.oM.Base;
+using BH.oM.Base;
 using SpeckleCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BHG = BH.oM.Geometry;
+using SCG = SpeckleCoreGeometryClasses;
 
 namespace BH.Engine.Speckle
 {
-  public static partial class Convert
-  {
+    public static partial class Convert
+    {
         /***************************************************/
         /**** Public Methods                            ****/
         /***************************************************/
+
+        // Mass point converter from SpeckleCoreGeometry, but with list rather than array        
+        /// <summary>
+        /// Mass point converter adapted from SpeckleCoreGeometry
+        /// </summary>
+        /// <param name = "arr" > Flat array of coordinates from Speckle </ param >
+        /// <returns>List of BHoM Points</returns>
+        public static List<BHG.Point> ToPoints(this IEnumerable<double> arr)
+        {
+            if (arr.Count() % 3 != 0) throw new Exception("Array malformed: length%3 != 0.");
+
+            List<BHG.Point> points = new List<BHG.Point>();
+            var asArray = arr.ToArray();
+            for (int i = 2, k = 0; i < arr.Count(); i += 3)
+                points[k++] = new BHG.Point { X = asArray[i - 2], Y = asArray[i - 1], Z = asArray[i] };
+
+            return points;
+        }
 
         public static IEnumerable<IBHoMObject> ToBHoM(ResponseObject response, bool setAssignedId = true, List<string> speckleIds = null)
         {
